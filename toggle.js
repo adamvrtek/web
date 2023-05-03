2de39376163fc299d4e46f117015d5db888fb791
Adam Vrtek <adam.vrtek.020@pslib.cz>
2023-05-03 12:19:04 +0200

    prices

---
+++ toggle.js
@@ -0,0 +1,8 @@
+function toggle() {
+    var x = document.getElementById("textToggle")
+    if (x.style.display === "block") {
+        x.style.display = "-webkit-box";
+    } else {
+        x.style.display = "block";
+    }
+}   
